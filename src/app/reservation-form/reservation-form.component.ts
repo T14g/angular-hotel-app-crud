3d837e16189921b257b17aa827a69a7658a7f42e
T14g <ottiagos@gmail.com>
2025-04-21 21:40:13 -0300

    navigate user when add reservation

--- src/app/reservation-form/reservation-form.component.ts
+++ src/app/reservation-form/reservation-form.component.ts
@@ -3,6 +3,7 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { OnInit } from '@angular/core';
 import { ReservationService } from '../reservation/reservation.service';
 import { Reservation } from '../models/reservation';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-reservation-form',
@@ -12,7 +13,7 @@ import { Reservation } from '../models/reservation';
 export class ReservationFormComponent implements OnInit {
   reservationForm: FormGroup = new FormGroup({});
 
-  constructor(private formBuilder: FormBuilder, private reservationService: ReservationService) { }
+  constructor(private formBuilder: FormBuilder, private reservationService: ReservationService, private router: Router) { }
 
   ngOnInit(): void {
     this.reservationForm = this.formBuilder.group({
@@ -29,6 +30,7 @@ export class ReservationFormComponent implements OnInit {
       const reservation: Reservation = this.reservationForm.value;
       this.reservationService.addReservation(reservation);
       console.log(localStorage.getItem('reservations'));
+      this.router.navigate(['/list']);
     }
   }
 }
